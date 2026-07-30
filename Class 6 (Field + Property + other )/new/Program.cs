e23d9a3a70fb4936a885b2bcf822c8be997613dc
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-07-30 21:21:46 +0600

    learn something new

---
+++ Class 6 (Field + Property + other )/new/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class User
+{
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        User user = new User
+        {
+            Name = "Asif",
+            Role = "Admin",
+            Age = 25
+        };
+
+        if (user is { Role: "Admin", Age: >= 18 and <= 60 })
+        {
+            Console.WriteLine("Access Granted");
+            Console.WriteLine($"Welcome {user.Name}");
+        }
+        else
+        {
+            Console.WriteLine("Access Denied");
+        }
+    }
+}
