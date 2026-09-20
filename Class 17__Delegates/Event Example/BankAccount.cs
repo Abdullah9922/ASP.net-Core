5406aaa29ad9ddd08267823982c1551e7384ddf4
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-09-21 00:52:16 +0600

    Event Example

---
+++ Class 17__Delegates/Event Example/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Event_Example
+{
+    public class BankAccount // amr kase akta class ase
+    {
+        public delegate void BalanceHandler(); // kon type ar methord nea amra kaj korbo?
+        public event BalanceHandler? MoneyDeposited; // money deposite hola amr ae event use korbo, then je kaj kora lage seta korbo
+
+        public void Deposit() // amr kase akta methord ase 
+        {
+            Console.WriteLine("Money deposited!"); //  methord  ter kaj korse (Money deposit korse)
+            MoneyDeposited?.Invoke(); // money deposit hola amake bolio to, amr kisu koj ase money deposite hola seta korbo ame
+        }
+
+        public void Aajira() // amr kase methord ase
+        {
+            Console.WriteLine("Kola pauruti."); // methord ter kaj korse. 
+        }
+
+        public void Amni() // amr kase methord ase
+        {
+            Console.WriteLine("Aro koto kaj thake oigula."); // methord ter kaj korse. 
+        }
+    }
+}
---
+++ Class 17__Delegates/Event Example/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using Event_Example;
+
+BankAccount account = new BankAccount(); // amr bank ame amr kaje use korsi.
+account.Aajira(); // amr eassa moto kaj korsi ame
+
+// money deposit hola amake bolba
+account.MoneyDeposited += () => { Console.WriteLine("Send notification to user."); };
+// money deposit hola ame ae kaj ta korbo. ame akta notification send korbo r ki
+// money deposit hola ame ae line ta print korbo tai ame seta event ar mordha raklam
+
+
+account.Aajira(); // onno je kono ja hosse
+account.Amni();
+
+// akon money deposit hola notification jebe
+// 2ta line print hosse dakho
+account.Deposit();
+// money deposit holo ae jonne Console.WriteLine("Send notification to user.") aetao print holo karon ame aeta event ar mordha raksilam.
+// ame caile aro kisu event ar mordha rakte partam
+
+// onno je kono kaj korle kintu notification send hobe na
+account.Aajira(); 
+account.Amni();
