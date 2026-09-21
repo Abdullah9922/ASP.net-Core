f94578db827645a335edb088074d565c51ed0919
abdullah9922 <ishtyaq.asif45@gmail.com>
2026-09-21 20:04:43 +0600

    Async Practice

---
+++ Class 20__ Threading + Asyne/Async Practice/Program.cs
@@ -0,0 +1,64 @@
+﻿public class Program
+{
+
+    public static async Task<string> GetStudentNameAsync()
+    {
+        // je kono kaj hote pare
+        await Task.Delay(2000);
+        return "Asif";
+    }
+
+    public static async Task<string> GetStudentDepartmentAsync()
+    {
+        await Task.Delay(2000);
+        return "CSEEEEEEE";
+    }
+
+    public static async Task Main(string[] args)
+    {
+        var result = await GetStudentNameAsync();
+        //Console.WriteLine(result);
+
+        Task<string> task1 = GetStudentNameAsync();
+        Task<string> task2 = GetStudentDepartmentAsync();
+
+        var result1 = await task1; // task1 ses hobe then baki kaj hobe
+        var result2 = await task2;
+
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+
+
+        //while(!task1.IsCompleted || !task1.IsCompleted)
+        //{
+        //    var completedTask = await Task.WhenAny(task1, task2); 
+
+        //    if(completedTask == task1)
+        //    {
+        //        var result11 = await task1; 
+        //    }
+
+        //    else if(completedTask == task2)
+        //    {
+        //        Console.WriteLine(await task2);
+        //    }
+        //}
+
+
+        // akon ame karo jonne wait korbo na. jeta age asbe ame seta print kora dibo
+
+        Task<string> completedTask = await Task.WhenAny(task1, task2);
+
+        Console.WriteLine(await completedTask);
+
+        if (completedTask == task1)
+        {
+            Console.WriteLine(await task2);  // task1 ba task2 je jaiga te use korbo sekhane await lagbe karon task1 r task2 asynconous mathord
+                                             
+        }
+        else
+        {
+            Console.WriteLine(await task1);
+        }
+    }
+}
---
+++ Class 20__ Threading + Asyne/Async example 1/Program.cs
@@ -0,0 +1,62 @@
+﻿public class Program
+{
+
+    public static async Task<string> OrderFoodAsync() // aeta amon akta methord jekhane amon akta kaj hossse jeta hota time lagte pare. koto time seta jani na
+    {
+        Console.WriteLine("Food order placed...");
+
+        await Task.Delay(3000); // ae khane ame delay disi but ae khane hoyto kono api thake data nea lagte pare ba database thake data fatch kora lagte pare
+
+        return "Burger";
+    }
+
+    public static async Task<string> OrderFromA_Async() // aeta amon akta methord jekhane amon akta kaj hossse jeta hota time lagte pare. koto time seta jani na
+    {
+        Console.WriteLine("Food order placed...");
+
+        await Task.Delay(3000); // ae khane ame delay disi but ae khane hoyto kono api thake data nea lagte pare ba database thake data fatch kora lagte pare
+
+        return "Pizza";
+    }
+
+    public static async Task<string> OrderFromB_Async() // aeta amon akta methord jekhane amon akta kaj hossse jeta hota time lagte pare. koto time seta jani na
+    {
+        Console.WriteLine("Food order placed...");
+
+        await Task.Delay(3000); // ae khane ame delay disi but ae khane hoyto kono api thake data nea lagte pare ba database thake data fatch kora lagte pare
+
+        return "Pizza";
+    }
+
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("App started");
+
+        var food = await OrderFoodAsync();
+
+        Console.WriteLine($"Received: {food}");
+
+
+        string food1 = await OrderFromA_Async();
+        string food2 = await OrderFromB_Async();
+
+        Task<string> task1 = OrderFromA_Async();
+        Task<string> task2 = OrderFromB_Async();
+
+        string food11 = await task1; // age task1 ses koro then samne agao
+        string food22 = await task2;
+        string[] foods = await Task.WhenAll(task1, task2); // age ae gula tast complite koro then samne jao to 
+    }
+
+    /*
+     
+     async  → "এই method asynchronous operation করতে পারে"
+
+    await  → "এই কাজ শেষ হওয়া পর্যন্ত অপেক্ষা করো,
+              কিন্তু thread অযথা block করো না"
+
+    Task   → "একটা চলমান/হওয়ার কথা থাকা asynchronous কাজকে represent করে"  
+     
+     */
+}
